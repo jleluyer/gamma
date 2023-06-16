c2dddb2572a82e27fa8ea8f30c6cb2a570a2f883
jleluyer <jeremy.le.luyer@gmail.com>
2023-06-15 17:10:54 -1000

    Update 02_prepare_bam.sh

--- 00_scripts/02_snps/datarmor_jobs/02_prepare_bam.sh
+++ 00_scripts/02_snps/datarmor_jobs/02_prepare_bam.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+
+# Clean past jobs
+
+rm 02_snps/datarmor_jobs/GATK_*sh
+
+
+# launch scripts
+for file in $(ls 04_mapped/*.concordant_uniq|perl -pe 's/.concordant_uniq//'|sort -u)
+
+do
+
+base=$(basename "$file")
+
+        toEval="cat 02_snps/02_gatk_prepare_bam_genome.pbs | sed 's/__BASE__/$base/g'"; eval $toEval > 02_snps/datarmor_jobs/GATK_$base.sh
+done
+
+#Submit jobs
+for i in $(ls 02_snps/datarmor_jobs/GATK_*sh); do qsub $i; done
 
