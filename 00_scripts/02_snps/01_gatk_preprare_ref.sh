e937462e0da36e27a12943a2e34fdfd807e50bf7
jleluyer <jeremy.le.luyer@gmail.com>
2023-06-15 17:04:35 -1000

    Update 01_gatk_preprare_ref.sh
    update

--- 00_scripts/02_snps/01_gatk_preprare_ref.sh
+++ 00_scripts/02_snps/01_gatk_preprare_ref.sh
@@ -2,7 +2,7 @@
 #PBS -N dict.__BASE__
 #PBS -o 98_log_files/gatk.prepare.ref.err
 #PBS -l walltime=23:00:00
-#PBS -l mem=70g
+#PBS -l mem=40g
 #PBS -r n
 
 cd $PBS_O_WORKDIR
@@ -10,12 +10,15 @@ cd $PBS_O_WORKDIR
 REFERENCE="path_to_genome_folder/Pmarg.genome.masked.fa"
 index="path_to_genome_folder/Pmarg.genome.masked.dict"
 
-TmpDir="gamma/gatk"
+TmpDir="04_mapped/gatk"
 GATK_jar="gatk-4.0.3.0/gatk-package-4.0.3.0-local.jar"
 
 #launch
-java -Xmx40G -Djava.io.tmpdir="$TmpDir" -Dsamjdk.use_async_io_read_samtools=false -Dsamjdk.use_async_io_write_samtools=true -Dsamjdk.use_async_io_write_tribble=false -Dsamjdk.compression_level=2 -jar "$GATK_jar" CreateSequenceDictionary \
+java -Xmx35G -Djava.io.tmpdir="$TmpDir" -Dsamjdk.use_async_io_read_samtools=false -Dsamjdk.use_async_io_write_samtools=true -Dsamjdk.use_async_io_write_tribble=false -Dsamjdk.compression_level=2 -jar "$GATK_jar" CreateSequenceDictionary \
 	-R $REFERENCE -O $index
 
+#Module load
+#Samtoolsv1.9
+
 # Samtools index fasta
-#samtools faidx $REFERENCE
+samtools faidx $REFERENCE
